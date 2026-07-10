afb6d20e0c1c5ee47d3c8b55d0652ab9db568a2b
maschin96 <markus.schindler@hochschule-burgenland.at>
2026-07-10 07:56:28 +0200

    Match Rust 1.79 formatting

--- rust/busybox-rs/src/coreutils.rs
+++ rust/busybox-rs/src/coreutils.rs
@@ -3,9 +3,6 @@ use core::ffi::{c_char, c_int};
 use crate::ffi::run_applet;
 
 #[no_mangle]
-pub extern "C" fn rust_true_main(
-    argc: c_int,
-    argv: *mut *mut c_char,
-) -> c_int {
+pub extern "C" fn rust_true_main(argc: c_int, argv: *mut *mut c_char) -> c_int {
     run_applet(argc, argv, |_| 0)
 }
